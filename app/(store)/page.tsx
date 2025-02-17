ee33a77b57bd0842b0c679681e7a8cb8e36b5c4d
Paulo Borges <victorborges296@hotmail.com>
2025-02-17 19:09:25 -0300

    ProductGrid, ProductView and ProductThumb components added + imageUrl helper function

--- app/(store)/page.tsx
+++ app/(store)/page.tsx
@@ -1,10 +1,19 @@
+import ProductsView from "@/components/ProductsView";
 import { Button } from "@/components/ui/button";
+import { getAllProducts } from "@/sanity/lib/products/getAllProducts";
+import { getAllCategories } from "@/sanity/lib/products/getAllCategories";
 
-export default function Home() {
+export default async function Home() {
+  const products = await getAllProducts()
+  const categories = await getAllCategories()
+
   return (
     <div>
-      <h1>hello 123</h1>
-      <Button>Click Me</Button>
+      <h1>Hello World</h1>
+
+      <div className="flex flex-col items-center justify-top min-h-screen bg-gray-100 p-4">
+        <ProductsView products={products} categories={categories} />
+      </div>
     </div>
   );
 }
